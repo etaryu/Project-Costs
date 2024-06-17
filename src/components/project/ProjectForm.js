15777ca30eaeca6d1ac194e3f5df516332b5a5c6
Yuggo <yuggoteruya1@gmail.com>
2024-06-17 11:00:03 -0400

    Connect API

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -15,7 +15,7 @@ function ProjectForm({btnText}){
                 'Content-Type':'application/json',
             }
         })
-        .then((resp) => resp.json())
+        .then((resp) =>  resp.json())
         .then((data) =>{
             setCategories(data)
         })
